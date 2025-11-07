2d58acb22f338c43799e1d8296af7a40f94e9a69
Divine Benjamin <divinebenjamin2004@gmail.com>
2025-11-07 03:33:39 +0100

    mvp done

--- main.js
+++ main.js
@@ -6,4 +6,26 @@ function setVH() {
 setVH();
 window.addEventListener('resize', setVH);
 
+function checkWidth(){
+  const width = window.innerWidth;
+  const screen = document.querySelector('body');
+  
+  if (width > 480){
+    const modalWrapper = `
+      <div class="modalWrapper">
+        <div class="modal">
+          <div class="modalImg">
+            <img src="../assets/images/404.svg" alt="404 img">
+          </div>
+          <h5 class="modalTitle">This App is Mobile Only</h5>
+          <p class="modalCaption">This experience was designed for smaller screens.<br>
+          Please view it on your mobile device.</p>
+        </div>
+      </div>
+    `
+    screen.innerHTML = modalWrapper;
+  };  
+};
 
+checkWidth();
+window.addEventListener('resize', checkWidth);
